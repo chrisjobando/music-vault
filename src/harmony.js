1b61082a455c2ef905f925dd371bab600b179784
Christopher Obando <chrisjobando@gmail.com>
2023-09-10 15:23:49 -0700

    update theme

--- src/harmony.js
+++ src/harmony.js
@@ -1,56 +1,57 @@
+
 export const harmony = {
-	name: 'harmony',
-	properties: {
+    name: 'harmony',
+    properties: {
 		// =~= Theme Properties =~=
 		"--theme-font-family-base": "system-ui",
 		"--theme-font-family-heading": "system-ui",
 		"--theme-font-color-base": "0 0 0",
 		"--theme-font-color-dark": "255 255 255",
-		"--theme-rounded-base": "9999px",
+		"--theme-rounded-base": "12px",
 		"--theme-rounded-container": "8px",
-		"--theme-border-base": "1px",
+		"--theme-border-base": "2px",
 		// =~= Theme On-X Colors =~=
 		"--on-primary": "0 0 0",
-		"--on-secondary": "255 255 255",
+		"--on-secondary": "0 0 0",
 		"--on-tertiary": "0 0 0",
 		"--on-success": "0 0 0",
 		"--on-warning": "0 0 0",
 		"--on-error": "255 255 255",
 		"--on-surface": "255 255 255",
 		// =~= Theme Colors  =~=
-		// primary | #0FBA81 
-		"--color-primary-50": "219 245 236", // #dbf5ec
-		"--color-primary-100": "207 241 230", // #cff1e6
-		"--color-primary-200": "195 238 224", // #c3eee0
-		"--color-primary-300": "159 227 205", // #9fe3cd
-		"--color-primary-400": "87 207 167", // #57cfa7
-		"--color-primary-500": "15 186 129", // #0FBA81
-		"--color-primary-600": "14 167 116", // #0ea774
-		"--color-primary-700": "11 140 97", // #0b8c61
-		"--color-primary-800": "9 112 77", // #09704d
-		"--color-primary-900": "7 91 63", // #075b3f
-		// secondary | #4F46E5 
-		"--color-secondary-50": "229 227 251", // #e5e3fb
-		"--color-secondary-100": "220 218 250", // #dcdafa
-		"--color-secondary-200": "211 209 249", // #d3d1f9
-		"--color-secondary-300": "185 181 245", // #b9b5f5
-		"--color-secondary-400": "132 126 237", // #847eed
-		"--color-secondary-500": "79 70 229", // #4F46E5
-		"--color-secondary-600": "71 63 206", // #473fce
-		"--color-secondary-700": "59 53 172", // #3b35ac
-		"--color-secondary-800": "47 42 137", // #2f2a89
-		"--color-secondary-900": "39 34 112", // #272270
-		// tertiary | #0EA5E9 
-		"--color-tertiary-50": "219 242 252", // #dbf2fc
-		"--color-tertiary-100": "207 237 251", // #cfedfb
-		"--color-tertiary-200": "195 233 250", // #c3e9fa
-		"--color-tertiary-300": "159 219 246", // #9fdbf6
-		"--color-tertiary-400": "86 192 240", // #56c0f0
-		"--color-tertiary-500": "14 165 233", // #0EA5E9
-		"--color-tertiary-600": "13 149 210", // #0d95d2
-		"--color-tertiary-700": "11 124 175", // #0b7caf
-		"--color-tertiary-800": "8 99 140", // #08638c
-		"--color-tertiary-900": "7 81 114", // #075172
+		// primary | #626EEF 
+		"--color-primary-50": "231 233 253", // #e7e9fd
+		"--color-primary-100": "224 226 252", // #e0e2fc
+		"--color-primary-200": "216 219 251", // #d8dbfb
+		"--color-primary-300": "192 197 249", // #c0c5f9
+		"--color-primary-400": "145 154 244", // #919af4
+		"--color-primary-500": "98 110 239", // #626EEF
+		"--color-primary-600": "88 99 215", // #5863d7
+		"--color-primary-700": "74 83 179", // #4a53b3
+		"--color-primary-800": "59 66 143", // #3b428f
+		"--color-primary-900": "48 54 117", // #303675
+		// secondary | #09A8FA 
+		"--color-secondary-50": "218 242 254", // #daf2fe
+		"--color-secondary-100": "206 238 254", // #ceeefe
+		"--color-secondary-200": "194 233 254", // #c2e9fe
+		"--color-secondary-300": "157 220 253", // #9ddcfd
+		"--color-secondary-400": "83 194 252", // #53c2fc
+		"--color-secondary-500": "9 168 250", // #09A8FA
+		"--color-secondary-600": "8 151 225", // #0897e1
+		"--color-secondary-700": "7 126 188", // #077ebc
+		"--color-secondary-800": "5 101 150", // #056596
+		"--color-secondary-900": "4 82 123", // #04527b
+		// tertiary | #41C5D3 
+		"--color-tertiary-50": "227 246 248", // #e3f6f8
+		"--color-tertiary-100": "217 243 246", // #d9f3f6
+		"--color-tertiary-200": "208 241 244", // #d0f1f4
+		"--color-tertiary-300": "179 232 237", // #b3e8ed
+		"--color-tertiary-400": "122 214 224", // #7ad6e0
+		"--color-tertiary-500": "65 197 211", // #41C5D3
+		"--color-tertiary-600": "59 177 190", // #3bb1be
+		"--color-tertiary-700": "49 148 158", // #31949e
+		"--color-tertiary-800": "39 118 127", // #27767f
+		"--color-tertiary-900": "32 97 103", // #206167
 		// success | #84cc16 
 		"--color-success-50": "237 247 220", // #edf7dc
 		"--color-success-100": "230 245 208", // #e6f5d0
@@ -95,5 +96,6 @@ export const harmony = {
 		"--color-surface-700": "55 68 107", // #37446b
 		"--color-surface-800": "44 54 86", // #2c3656
 		"--color-surface-900": "36 44 70", // #242c46
+		
 	}
 }
