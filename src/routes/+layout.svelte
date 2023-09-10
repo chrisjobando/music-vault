4eb12e1e96ae91688f9af74bc5564351595459e3
Christopher Obando <chrisjobando@gmail.com>
2023-09-10 12:30:28 -0700

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,25 @@
 <script>
+	// Global CSS
 	import '../app.postcss';
-
+	// Skeleton UI
+	import { AppBar, AppShell, storePopup } from '@skeletonlabs/skeleton';
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { AppShell, storePopup } from '@skeletonlabs/skeleton';
+	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
+
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<div slot="lead">NavIcon</div>
+			<div>Title</div>
+			<div slot="trail">Avatar</div>
+		</AppBar>
+	</svelte:fragment>
 
-<AppShell>
-	<svelte:fragment slot="header">Header</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+
 	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
 	<slot />
 	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
