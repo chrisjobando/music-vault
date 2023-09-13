1797a833cbe451c6ddd1091b6798c1081ca0e3d4
Christopher Obando <chrisjobando@gmail.com>
2023-09-13 14:46:40 -0700

    create signout flow

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,7 @@ export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
   const session = await getSession();
 
   if (!session) {
-    throw redirect(303, '/login');
+    throw redirect(303, '/auth/signin');
   }
 
   return { url: url.origin };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
 <h1>Welcome!</h1>
+
+<form action="/auth/signout" method="POST">
+  <button class="variant-filled-warning btn">Sign Out</button>
+</form>
