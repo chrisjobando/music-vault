727b0a6c1a5368c5a70660551288a9275e0ab7c8
Christopher Obando <chrisjobando@gmail.com>
2023-09-13 19:33:13 -0700

    remove unecessary path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import * as path from 'path';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
   plugins: [sveltekit(), purgeCss()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+    alias: {}
   }
 });
