5e7b0b1dc4770bdafb0b6173f997fb70bb0bdde0
Christopher Obando <chrisjobando@gmail.com>
2023-09-13 19:01:14 -0700

    add callback redirect prop

--- src/routes/(auth)/auth/callback/+server.ts
+++ src/routes/(auth)/auth/callback/+server.ts
@@ -4,10 +4,13 @@ export const GET = async ({ url, locals: { supabase } }) => {
   // Get the code from the auth server
   const code = url.searchParams.get('code');
 
+  // Next page for redirect
+  const next = url.searchParams.get('next') ?? '/';
+
   if (code) {
     // Get a session in exchange for the code and store it in our cookies
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  throw redirect(303, '/');
+  throw redirect(303, next);
 };
