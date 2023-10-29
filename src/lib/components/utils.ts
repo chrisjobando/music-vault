43c9385e8630e4ecbe127302f3ba7b5fa5f8a19f
Christopher Obando <chrisjobando@gmail.com>
2023-10-29 09:42:34 -0700

    component color utils

--- src/lib/components/utils.ts
+++ src/lib/components/utils.ts
@@ -1,5 +1,13 @@
 export type IAlertType = 'info' | 'success' | 'warning' | 'error';
+
 export type IButtonType = IAlertType | 'primary' | 'secondary';
+
+export const alertColors: { [key in IAlertType]: string } = {
+  info: 'alert-info',
+  success: 'alert-success',
+  warning: 'alert-warning',
+  error: 'alert-error'
+};
 
 export const buttonColors: { [key in IButtonType]: string } = {
   primary: 'btn-primary',
@@ -7,5 +15,14 @@ export const buttonColors: { [key in IButtonType]: string } = {
   warning: 'btn-warning',
   success: 'btn-success',
   info: 'btn-info',
-  error: 'btn-danger'
+  error: 'btn-error'
+};
+
+export type IMarginType = 'sm' | 'md' | 'lg' | 'xl';
+
+export const marginTop: { [key in IMarginType]: string } = {
+  sm: 'mt-2',
+  md: 'mt-4',
+  lg: 'mt-8',
+  xl: 'mt-10'
 };
