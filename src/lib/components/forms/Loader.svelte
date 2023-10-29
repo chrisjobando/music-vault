4885ca357d1cc9f2e670ac66d02361cc58cc10af
Christopher Obando <chrisjobando@gmail.com>
2023-10-29 09:50:55 -0700

    use daisy class

--- src/lib/components/forms/Loader.svelte
+++ src/lib/components/forms/Loader.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
   export let loading = false;
-
-  /**
-   * @description Dialog ref object
-   */
-  let spinnerRef: HTMLDialogElement | null = null;
-
-  $: if (spinnerRef && loading) spinnerRef.showModal();
 </script>
 
-<dialog bind:this={spinnerRef}>
-  {#if spinnerRef && loading}
-    <span class="loading loading-spinner loading-lg" />
-  {/if}
-</dialog>
+{#if loading}
+  <span class="loading loading-spinner loading-lg text-primary" />
+{/if}
