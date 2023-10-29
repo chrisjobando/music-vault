56dd93863436b0e87e25dba4460f53ecc3a8d661
Christopher Obando <chrisjobando@gmail.com>
2023-10-28 22:08:21 -0700

    remove additional pages

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -5,8 +5,7 @@ interface IRoute {
 
 export const Routes: IRoute[] = [
   { name: 'Home', path: '/' },
-  { name: 'Collection', path: '/collection' },
-  { name: 'Rotation', path: '/rotation' }
+  { name: 'Collection', path: '/collection' }
 ];
 
 export const partialAuthRoutes: string[] = ['/auth/create-profile', '/auth/signout'];
