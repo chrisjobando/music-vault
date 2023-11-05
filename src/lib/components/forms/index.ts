22d2bdc14d73dc70ec6febc8d33e2b1be6ebf66e
Christopher Obando <chrisjobando@gmail.com>
2023-11-05 11:15:54 -0800

    add file input to form components

--- src/lib/components/forms/index.ts
+++ src/lib/components/forms/index.ts
@@ -1,5 +1,6 @@
 export { default as Alert } from './Alert.svelte';
 export { default as EmailInput } from './EmailInput.svelte';
+export { default as FileInput } from './FileInput.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as NumberInput } from './NumberInput.svelte';
 export { default as PasswordInput } from './PasswordInput.svelte';
