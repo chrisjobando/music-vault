d1758405157deaa7d5b00aaeedae3c520838a300
Christopher Obando <chrisjobando@gmail.com>
2023-11-05 23:20:21 -0800

    restyle avatar

--- src/lib/components/profile/Avatar.svelte
+++ src/lib/components/profile/Avatar.svelte
@@ -2,9 +2,16 @@
   // Interfaces
   import type { IProfiles } from '$lib/types';
   // Utils
+  import cx from 'classnames';
   import { getUserInitials } from './utils';
 
   // Props
+  /**
+   * @description Avatar size
+   * @default 'md'
+   */
+  export let size: 'xs' | 'sm' | 'md' | 'lg' = 'md';
+
   /**
    * @description User's account data
    */
@@ -14,9 +21,34 @@
 </script>
 
 {#if !userProfile.avatar_url}
-  <div>
-    <p>{userInitials}</p>
+  <div class="avatar placeholder">
+    <div
+      class={cx('rounded-full bg-neutral-focus text-neutral-content', {
+        'w-10': size === 'xs',
+        'w-12': size === 'sm',
+        'w-16': size === 'md',
+        'w-24': size === 'lg'
+      })}
+    >
+      <span
+        class={cx({
+          'text-xs': size === 'xs',
+          'text-xl': size === 'md',
+          'text-3xl': size === 'lg'
+        })}>{userInitials}</span
+      >
+    </div>
   </div>
 {:else}
-  <img src={userProfile.avatar_url} alt="User's Avatar" />
+  <div class="avatar">
+    <div
+      class={cx('rounded-full ring ring-primary ring-offset-2 ring-offset-base-100', {
+        'w-12': size === 'sm',
+        'w-16': size === 'md',
+        'w-24': size === 'lg'
+      })}
+    >
+      <img src={userProfile.avatar_url} alt="User's Avatar" />
+    </div>
+  </div>
 {/if}
