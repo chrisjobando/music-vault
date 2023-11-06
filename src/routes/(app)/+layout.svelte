739e765c5ce44a603edd06efde2be5e34dd089e4
Christopher Obando <chrisjobando@gmail.com>
2023-11-06 07:40:51 -0800

    Add footer

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -11,11 +11,20 @@
   export let data;
 
   const { userProfile } = data;
+
+  const year = new Date().getFullYear();
 </script>
 
 {#if $page.route.id && userProfile}
-  <div>
+  <div class="flex min-h-screen flex-col">
     <Navbar {userProfile} />
-    <div class="mx-auto max-w-6xl p-10"><slot /></div>
+
+    <div class="mx-auto w-full max-w-6xl p-10"><slot /></div>
+
+    <footer class="footer mt-auto bg-base-200 p-4 text-base-content">
+      <aside>
+        <p>Copyright © {year} - All rights reserved by Christopher Obando</p>
+      </aside>
+    </footer>
   </div>
 {/if}
