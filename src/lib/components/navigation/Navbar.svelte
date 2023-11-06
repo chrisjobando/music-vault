e2688982ae7de0a792d19b391646922e256f34fd
Christopher Obando <chrisjobando@gmail.com>
2023-11-06 07:41:12 -0800

    fix mobile small drawer

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -20,8 +20,7 @@
 <div class="drawer">
   <input id="side-drawer" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex flex-col">
-    <!-- Navbar -->
-    <nav class="navbar w-full bg-base-200">
+    <nav class="navbar sticky w-full bg-base-200">
       <div class="flex-none lg:hidden">
         <label for="side-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
           <svg
@@ -39,13 +38,12 @@
         </label>
       </div>
 
-      <div class="mx-2 flex-1 px-2">
+      <div class="flex-none px-2">
         <a class="btn btn-ghost text-xl normal-case" href="/">Music Match</a>
       </div>
 
-      <div class="hidden flex-none lg:block">
+      <div class="hidden flex-1 lg:flex">
         <ul class="menu menu-horizontal">
-          <!-- Navbar menu content here -->
           {#each Routes as route}
             <li>
               <a
@@ -53,13 +51,15 @@
                 class={cx({
                   'font-bold': $page.url.pathname === route.path,
                   'font-normal': $page.url.pathname !== route.path
-                })}>{route.name}</a
+                })}
               >
+                {route.name}
+              </a>
             </li>
           {/each}
         </ul>
 
-        <div class="dropdown dropdown-end">
+        <div class="dropdown-end dropdown ml-auto">
           <div class="flex items-center">
             <button class="btn btn-circle btn-ghost">
               <Avatar size="xs" {userProfile} />
@@ -90,10 +90,20 @@
     </nav>
   </div>
 
-  <div class="drawer-side">
+  <div class="drawer-side z-50">
     <label for="side-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu min-h-full w-80 bg-base-100 p-4">
-      <!-- Sidebar content here -->
+      <li>
+        <a
+          href="/"
+          class={cx('text-xl', {
+            'font-bold': $page.url.pathname === '/',
+            'font-normal': $page.url.pathname !== '/'
+          })}
+        >
+          Home
+        </a>
+      </li>
       {#each Routes as route}
         <li>
           <a
@@ -106,6 +116,32 @@
           </a>
         </li>
       {/each}
+
+      <div class="dropdown dropdown-top mt-auto">
+        <div class="flex items-center">
+          <button class="btn btn-circle btn-ghost">
+            <Avatar size="xs" {userProfile} />
+          </button>
+          <p class="mx-2">{userProfile.display_name}</p>
+        </div>
+
+        <ul class="menu dropdown-content rounded-box menu-sm z-10 mb-3 w-52 bg-base-200 p-2 shadow">
+          <li>
+            <a
+              class={cx({
+                'font-bold': $page.url.pathname.includes('/profile'),
+                'font-normal': !$page.url.pathname.includes('/profile')
+              })}
+              href="/profile"
+            >
+              Profile
+            </a>
+          </li>
+          <li>
+            <form method="POST" action="/auth/signout" use:enhance>Logout</form>
+          </li>
+        </ul>
+      </div>
     </ul>
   </div>
 </div>
