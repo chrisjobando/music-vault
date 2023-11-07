7b3aa6f82d6f8539aeee4983da3cc67923c41ee9
Christopher Obando <chrisjobando@gmail.com>
2023-11-06 22:01:25 -0800

    extract alert icon svgs

---
+++ src/lib/components/forms/alertIcons/index.ts
@@ -0,0 +1,4 @@
+export { default as ErrorIcon } from './ErrorIcon.svelte';
+export { default as InfoIcon } from './InfoIcon.svelte';
+export { default as SuccessIcon } from './SuccessIcon.svelte';
+export { default as WarningIcon } from './WarningIcon.svelte';
